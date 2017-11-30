bb23b6054a69cfd13cad6c7b2719a680e63ad380
tylercable1 <tylercable7@gmail.com>
2017-11-30 11:43:07 -0800

    breakthrough on this

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,25 +2,30 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
-  var storage = {};
+  someInstance.storage = {};
+  someInstance.counter = 0;
   _.extend(someInstance, queueMethods);
   return someInstance;
 };
 
 var queueMethods = {};
 
+
 queueMethods.dequeue = function() {
 // use this here
+
 };
 
 queueMethods.enqueue = function(value) {
 // use this here
+  this.storage = value;
+  this.counter++;
 };
 
 queueMethods.size = function() {
 // use this here
  
-  return Object.keys().length;
+  return Object.keys(this.storage).length;
 };
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
+var storage = {};
+
+stackMethods.push = function(value) {
+
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+  return Object.keys(storage).length;
+};
 
 
