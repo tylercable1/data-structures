40fa8be75cf506be3556dcc04a95649dffbde8fb
tylercable1 <tylercable7@gmail.com>
2017-12-01 13:27:22 -0800

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; //empty array?  // fix me
 
   return newTree;
 };
@@ -11,9 +11,33 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+//use this
+  this.children.push(Tree(value));
+
 };
 
 treeMethods.contains = function(target) {
+//use recursion
+// use this?
+
+// check if this.value === target if yes return true
+// else if this.children.length < 0? return false
+// else for loop i = 0; 
+// if this.children[i].value === target return true
+// if  this.children[i].children.length > 0? 
+// return this.contains(target, this.children)
+
+  if (this.value === target) {
+    return true;
+  } 
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target) === true) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
