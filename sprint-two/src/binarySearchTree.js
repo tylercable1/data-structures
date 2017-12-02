5b615828a6ced783fcb0091fe3d4663d6c6cec2f
tylercable1 <tylercable7@gmail.com>
2017-12-02 11:41:32 -0800

    defining our search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,23 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+BinarySearchTree.prototype.insert = function(value) {
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+
+};
