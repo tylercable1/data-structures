fa3254fda2aded8da4b20e26705be91e64721f8e
tylercable1 <tylercable7@gmail.com>
2017-12-01 21:52:20 -0800

    finished set, started hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,14 +3,26 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k, v];
+  if (this._storage.get(index) === undefined) {
+    var bucket = [];
+    bucket.push(tuple);
+    this._storage.set(index, bucket);
+  } else {
+    bucket.push(tuple); 
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  return this._storage.get(index);
+
 };
 
 HashTable.prototype.remove = function(k) {
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.indexOf(item === -1)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+
+  return this._storage.indexOf(item) > -1;
 };
 
 setPrototype.remove = function(item) {
+
+  var removalIndex = this._storage.indexOf(item);
+  this._storage.splice(removalIndex, 1);
 };
 
 /*
