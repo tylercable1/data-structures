9ac3597b08f690d002f610735b0bddbaf13fcb34
tylercable1 <tylercable7@gmail.com>
2017-12-03 23:02:34 -0800

    finished binary search tree using pseudoclassical style

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -12,15 +12,45 @@ var BinarySearchTree = function(value) {
 
 BinarySearchTree.prototype.insert = function(value) {
   // check new value against original val
-  // if > assign new instance to this.right
+  // if > and .right is null assign new instance to this.right
   // if < assign to this.left  
+  if (value < this.value) {
+  	if (this.left === null) {
+  		this.left = new BinarySearchTree(value);
+  	} else {
+  	  this.left.insert(value);	
+  	}
+  } else if (value > this.value) {
+  	if (this.right === null) {
+  		this.right = new BinarySearchTree(value);
+  	} else {
+  	  this.right.insert(value);	
+  	}
+  } else {
 
+  }
 };
 
 BinarySearchTree.prototype.contains = function(value) {
-
+  // compare passed in value to new instance value if equal return true
+  // check if less than or greater than current nodes value
+  // check if .left or .right properties are null if null, return false
+  // and if not recurse until value either matches or null is found
+  if (value === this.value) {
+  	return true;
+  } else if (value < this.value) {
+  	return !! (this.left && this.left.contains(value));
+  } else if (value > this.value) {
+  	return !! (this.right && this.right.contains(value));
+  }
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb) {
-
+  cb(this.value);
+  if (this.left) {
+  	this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+  	this.right.depthFirstLog(cb);
+  }
 };
